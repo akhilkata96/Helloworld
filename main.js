9f367e198d1958692c69038ccbf9ad6ecbdba44f
akhilkata96 <kata.akhil@gmail.com>
2021-02-27 19:36:20 -0500

    Helloworld

---
+++ main.js
@@ -0,0 +1,34 @@
+1)var carName = "Volvo"; //variables ex1
+2)var carName = "Volvo";
+3)var x = 5;
+ var y = 10;
+ document.getElementById("demo").innerHTML = x + y;
+4)var x = 5;
+  var y = 10;
+  var z = x + y;
+  alert(z);
+5)var firstName = "John", lastName = "Doe",age = 35;
+Operators
+---------
+6)alert(10 * 5);  //Multiply 10 with 5, and alert the result:
+7)alert(10 / 2); //Divide 10 by 2, and alert the result:
+8)alert(15 % 9);  //Alert the remainder when 15 is divided by 9.
+9)x = 10;
+  y = 5;
+  x += y; //Use the correct assignment operator that will result in x being 15 (same as x = x + y).
+10)x = 10;
+   y = 5;
+   x * = y;  //Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+11)var length = 16;          // Number
+   var lastName = "Johnson"; // String
+   var x = {
+        firstName: "John",    //Use comments to describe the correct data type of the following variables:
+        lastName: "Doe"
+           };                        // Object
+12)function myFunction() {
+   alert("Hello World!");}   //Execute the function named myFunction.
+   myFunction();
+13)function myFunction(){
+  alert("Hello World!");
+   }				//Create a function called "myFunction".
+
